981d06240fbd8b55cb386ba34b95fa965151aabd
Sandu01 <sandusanf01@gmail.com>
2023-07-24 09:48:50 +0530

    database function changes

--- frontend/test/teacher-add.test.js
+++ frontend/test/teacher-add.test.js
@@ -8,9 +8,9 @@ test('Testing add teachers', async t => {
     await t.navigateTo("/dbinitialize");
 
     await t.navigateTo("/addTeacher");
-    await t.typeText("#teacher-id", "123456");
-    await t.typeText("#teacher-name", "Mohan Perera");
-    await t.typeText("#teacher-age", "45");
+    await t.typeText("#teacher-id", "10033");
+    await t.typeText("#teacher-name", "Nilanthi Fernando");
+    await t.typeText("#teacher-age", "42");
     await t.click("#teacher-add");
 
     await t.navigateTo("/teacher");
@@ -19,5 +19,5 @@ test('Testing add teachers', async t => {
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    await t.expect(tdText).contains("Mohan Perera");
+    await t.expect(tdText).contains("Nilanthi Fernando");
 });
